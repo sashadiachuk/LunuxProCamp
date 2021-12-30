0a691f5facdc0a0591906561de60dcddc3a842fb
sashadiachuk <alex170104@gmail.com>
2021-12-30 17:45:43 +0200

    hw6 started

---
+++ HW6/L06-examples/timer.c
@@ -0,0 +1,76 @@
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/ktime.h>
+#include <linux/timer.h>
+
+MODULE_LICENSE("GPL");
+
+static struct timer_list my_timer;
+static unsigned long start;
+
+static int restart = 5;
+static unsigned long delay_in_jiffies = (HZ * 200L) / MSEC_PER_SEC;
+
+static void timer_callback(struct timer_list *timer)
+{
+	pr_info("timer_callback called (%lu).\n", jiffies);
+
+
+   //Add button question action
+   //і якщо вна натиснена, то вмикає світлодіод, інакше вимикає.  
+
+
+	if (restart--) {
+		start += delay_in_jiffies;
+		mod_timer(timer, start);
+	}
+
+	/*
+	       Re-enable timer. Because this function will be called only first time. 
+       		If we re-enable this will work like periodic timer. 
+    
+   			 mod_timer(timer, jiffies + msecs_to_jiffies(TIMEOUT));
+	
+	
+	*/
+
+
+}
+
+static __init int timer_init(void)
+{
+	unsigned long now;
+
+	pr_info("Timer module installing\n");
+
+	timer_setup(&my_timer, timer_callback, 0);
+
+	now = jiffies;
+	start = now + delay_in_jiffies;
+	pr_info("Starting timer to fire in %lu ms (%lu)\n", start, now);
+
+	mod_timer(&my_timer, start);
+
+
+	return 0;
+}
+
+static __exit void timer_exit(void)
+{
+	if (del_timer(&my_timer))
+		pr_info("Рendng timer deleted\n");
+
+	pr_info("Timer module uninstalling\n");
+
+	return;
+}
+
+
+
+
+module_init(timer_init);
+module_exit(timer_exit);
+
+
+
+
